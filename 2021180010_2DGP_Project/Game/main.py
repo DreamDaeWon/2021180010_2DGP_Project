83892a03582757493ea664e26cae40111b0580b5
DreamDaeWon@NoteBookOmen <142958544+DreamDaeWon@users.noreply.github.com>
2024-10-16 13:05:28 +0900

    24 10 16 13:05 Player file Create and main struct create

---
+++ 2021180010_2DGP_Project/Game/main.py
@@ -0,0 +1,37 @@
+from pico2d import*
+
+
+
+
+def handle_events():
+    pass
+
+
+def initialize():
+    pass
+
+def update():
+    pass
+
+def late_update():
+    pass
+
+
+def rander_world():
+    pass
+
+
+
+
+open_canvas()
+initialize()
+
+running = True
+
+
+# Game Loop~
+while running:
+    handle_events()
+    update()
+    late_update()
+    rander_world()
