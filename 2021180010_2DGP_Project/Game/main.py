5a0528006783cc0369594bef68813496ec3094a0
DreamDaeWon@NoteBookOmen <142958544+DreamDaeWon@users.noreply.github.com>
2024-10-16 13:08:44 +0900

    24 10 16 13:08 handl_evenets func 제작 x누르면 나가도록 제작

--- 2021180010_2DGP_Project/Game/main.py
+++ 2021180010_2DGP_Project/Game/main.py
@@ -4,6 +4,13 @@ from pico2d import*
 
 
 def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
     pass
 
 
