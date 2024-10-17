0d24e0380e8ded294c5f15067673bdd73ab98295
DreamDaeWon@NoteBookOmen <142958544+DreamDaeWon@users.noreply.github.com>
2024-10-17 16:42:12 +0900

    24 10 17 16:41 Player Hit render plus

--- 2021180010_2DGP_Project/Game/CupHeadBanging/CupheadBanging.py
+++ 2021180010_2DGP_Project/Game/CupHeadBanging/CupheadBanging.py
@@ -4,21 +4,29 @@ from pico2d import*
 class CupheadBainging:
     def __init__(self):
         self.frame = 0
-
+        # 리소스 기본상태
         path = 'CupHeadBanging/PlayerResoures/Idle/cuphead_idle_000' # main.py 기준임
         self.image_Idle = []
         for a in range(1,5+1):
             finalPath = path + str(a) + '.png'
             self.image_Idle.append(load_image(finalPath))
 
+        # 리소스 기본상태
+        path = 'CupHeadBanging/PlayerResoures/Hit/cuphead_hit_air_000'  # main.py 기준임
+        self.image_Hit = []
+        for a in range(1, 6+1):
+            finalPath = path + str(a) + '.png'
+            self.image_Hit.append(load_image(finalPath))
 
-        self.maxFrame = 5
+
+        self.idle_maxFrame = 5
+        self.hit_maxFrame = 6
 
 
 
     def update(self):
         self.frame += 1
-        if self.frame >= self.maxFrame:
+        if self.frame >= self.hit_maxFrame:
             self.frame = 0
 
         pass
@@ -28,5 +36,5 @@ class CupheadBainging:
         pass
 
     def render(self):
-        self.image_Idle[self.frame].draw(100,100)
+        self.image_Hit[self.frame].draw(100,100)
         pass
