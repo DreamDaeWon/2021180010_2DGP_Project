940f3dc47c1bda68e072aeeef1f412d711c7569c
DreamDaeWon@NoteBookOmen <142958544+DreamDaeWon@users.noreply.github.com>
2024-10-22 01:09:11 +0900

    24 10 22 01:08 Collision Create

---
+++ 2021180010_2DGP_Project/Game/collision.py
@@ -0,0 +1,17 @@
+from dw_define import*
+
+class Collision:
+    def __init__(self):
+        self.all_object = 0 # 모든 객체를 받아올 것임
+
+        self.player = 0 # 플레이어 객체를 받아올 것임
+
+        self.player_bullet = 0 # 플레이어 총알 객체 리스트를 받아올 것임
+
+        self.boss = 0 # 보스 객체를 받아 올 것임
+
+        self.boss_bullet = 0 # 보스의 총알 객체 리스트를 받아올 것임
+
+        pass
+    
+
