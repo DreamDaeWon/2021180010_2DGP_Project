bcb4f3250e996dd2d832ba2dbc58eadee07be44c
DreamDaeWon@NoteBookOmen <142958544+DreamDaeWon@users.noreply.github.com>
2024-11-02 23:23:18 +0900

    24 11 02 All Boss class update

--- 2021180010_2DGP_Project/Game/BossPotato/boss_potato.py
+++ 2021180010_2DGP_Project/Game/BossPotato/boss_potato.py
@@ -24,24 +24,44 @@ class Boss_Potato:
         self.rundir = 0
         self.run_speed = 10
 
+        self.before_state_tuple = 0
+        self.now_state_tuple = 0
     pass
 
 
-def in_put_resources(self):
+    def in_put_resources(self):
         # 리소스 기본상태
-    path = 'CupHeadBanging/PlayerResoures/Idle/cuphead_idle_000'  # main.py 기준임
-    self.image_Idle = []
-    for a in range(1, 8 + 1):
-        finalPath = path + str(a) + '.png'
-        self.image_Idle.append(load_image(finalPath))
+        path = 'CupHeadBanging/PlayerResoures/Idle/cuphead_idle_000'  # main.py 기준임
+        self.image_Idle = []
+        for a in range(1, 8 + 1):
+            finalPath = path + str(a) + '.png'
+            self.image_Idle.append(load_image(finalPath))
 
-def boss_state_updete(self):  # 보스 상태가 변경 될 때 해주 어야 할 것들
-    if self.before_state_tuple != self.now_state_tuple:
-        self.frame = 0
+    def boss_state_update(self):  # 보스 상태가 변경 될 때 해주 어야 할 것들
+        if self.before_state_tuple != self.now_state_tuple:
+            self.frame = 0
+            pass
+
+
+    def boss_move(self):
         pass
 
 
+    def boss_resource_state(self):
+
+        self.before_state_tuple = self.now_state_tuple
+
+        self.now_state_tuple = self.idle
+
+        if self.before_state_tuple == self.now_state_tuple:
+            self.frame += 1 * self.now_state_tuple[1]
+            if self.frame >= self.now_state_tuple[0]:
+                self.frame = 0
+        else:
+            self.boss_state_update()
+            print('in this')
 
 
 
+        pass
 
