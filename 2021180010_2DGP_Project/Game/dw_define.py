68a79445543a64cf506d34aee4f4b50398073518
DreamDaeWon@NoteBookOmen <142958544+DreamDaeWon@users.noreply.github.com>
2024-11-11 10:06:45 +0900

    24 11 11 Object Manager create

--- 2021180010_2DGP_Project/Game/dw_define.py
+++ 2021180010_2DGP_Project/Game/dw_define.py
@@ -2,3 +2,4 @@ from pico2d import*
 from enum import*
 import math
 from CupHeadBanging.cupheadBanging import CupheadBanging
+from BossPotato.boss_potato import Boss_Potato
---
+++ 2021180010_2DGP_Project/Game/object_manager.py
@@ -0,0 +1,4 @@
+from dw_define import*
+
+world = [] # 모든 세상 객체들
+
